55c4b2d359f44a46186a7431ca42ce2674a6a598
Matthew Johnson <matthewjohnson@snarkydata.com>
2023-01-16 18:16:34 -0600

    Adding Lecture 24 files and directories

---
+++ Lecture-24-Comments/main.c
@@ -0,0 +1,15 @@
+/*
+    Author: Matt Johnson
+    Purpose: Program prints name to the screen
+    Date: 01/16/2023
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+
+    // code displays my name
+    printf("Hi, my name is Matthew!\n");
+    return 0;
+}
